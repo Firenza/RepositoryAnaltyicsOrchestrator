7f3be46768d46987de2b6a0a9728c8b142e9c207
Firenza <chadoftedahl@gmail.com>
2018-02-13 11:10:20 -0600

    Add project files.

---
+++ RepositoryAnaltyicsDataRefresher/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RepositoryAnaltyicsDataRefresher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
